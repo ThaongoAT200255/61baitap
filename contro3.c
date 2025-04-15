6cbca90ffca1032df5f08aade078555a35e17a87
ThaongoAT200255 <thaongo8105@gmail.com>
2025-04-15 15:51:37 +0700

    Add files via upload

--- contro3.c
+++ contro3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    int a[100], n;
+    int *p;
+
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
+
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+
+    p = a;  // trỏ p đến đầu mảng
+
+    int min = *p;
+    int max = *p;
+
+    for (int i = 1; i < n; i++) {
+        if (*(p + i) < min) {
+            min = *(p + i);
+        }
+        if (*(p + i) > max) {
+            max = *(p + i);
+        }
+    }
+
+    printf("\nGia tri nho nhat trong mang: %d\n", min);
+    printf("Gia tri lon nhat trong mang: %d\n", max);
+
+    return 0;
+}
--- contro4.c
+++ contro4.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+void sapXepTangDan(int *a, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (*(a + i) > *(a + j)) {
+                int temp = *(a + i);
+                *(a + i) = *(a + j);
+                *(a + j) = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int a[100], n;
+
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
+
+    printf("Nhap cac phan tu cua mang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+
+    // Gọi hàm sắp xếp
+    sapXepTangDan(a, n);
+
+    // In kết quả
+    printf("\nMang sau khi sap xep tang dan:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", *(a + i));
+    }
+
+    printf("\n");
+    return 0;
+}
